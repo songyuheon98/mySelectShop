b40dfc5012ddebf5b44657899a793e417322a9df
songyuheon98 <songyuheon2750@gmail.com>
2023-09-06 15:58:26 +0900

    4주차 마무리

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -14,4 +14,10 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
 
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+    /**
+     * select * from product p left join product_folder pf on p.id = pf.product_id
+     * where p.user_id=1 and pf.folder_id = 4
+     * order by p.id desc
+     * limit 10, 10;
+     */
 }
