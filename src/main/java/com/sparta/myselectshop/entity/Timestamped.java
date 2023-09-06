511cf5c9e89c73bcdd8e895a25a35cabad9cfd1a
songyuheon98 <songyuheon2750@gmail.com>
2023-09-06 11:01:33 +0900

    관심상품 등록

---
+++ src/main/java/com/sparta/myselectshop/entity/Timestamped.java
@@ -0,0 +1,30 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+
+@EnableJpaAuditing
+@Setter
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
