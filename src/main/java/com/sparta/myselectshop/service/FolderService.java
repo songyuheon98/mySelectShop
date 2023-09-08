817da76d47b59e178ab941d95f095ead3215f4c9
songyuheon98 <songyuheon2750@gmail.com>
2023-09-08 12:07:57 +0900

    @ExceptionHandler 적용

--- src/main/java/com/sparta/myselectshop/service/FolderService.java
+++ src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -28,7 +28,7 @@ public class FolderService {
                 folderList.add(folder);
             }
             else{
-                throw new IllegalArgumentException("폴더명이 중복되었습니다.");
+                throw new IllegalArgumentException("중복된 폴더명을 제거해주세요 : "+folderName+" 폴더");
             }
         }
         folderRepository.saveAll(folderList);
